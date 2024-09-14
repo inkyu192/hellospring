098143b307e44b58b382caef52b2f623a4b3db2f
inkyu192 <inkyu192@gmail.com>
2024-09-14 12:59:42 +0900

    클래스의 분리

--- src/main/java/tobyspring/hellospring/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/WebApiExRateProvider.java
@@ -10,9 +10,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.stream.Collectors;
 
-public class WebApiExRatePaymentService extends PaymentService {
+public class WebApiExRateProvider {
 
-    @Override
     public BigDecimal getExRate(String currency) throws IOException {
         URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -22,7 +21,6 @@ public class WebApiExRatePaymentService extends PaymentService {
 
         ObjectMapper mapper = new ObjectMapper();
         ExRateDate data = mapper.readValue(response, ExRateDate.class);
-        BigDecimal exRate = data.rates().get("KRW");
-        return exRate;
+        return data.rates().get("KRW");
     }
 }
