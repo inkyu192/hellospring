871aeeadef8291ac307cb9c3d9f4acea4f617d7e
inkyu192 <inkyu192@gmail.com>
2024-10-03 18:59:14 +0900

    테스트와 DI (2)

---
+++ src/test/java/tobyspring/hellospring/TestObjectFactory.java
@@ -0,0 +1,27 @@
+package tobyspring.hellospring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tobyspring.hellospring.exrate.CachedExRateProvider;
+import tobyspring.hellospring.exrate.WebApiExRateProvider;
+import tobyspring.hellospring.payment.ExRateProvider;
+import tobyspring.hellospring.payment.ExRateProviderStub;
+import tobyspring.hellospring.payment.PaymentService;
+
+import java.math.BigDecimal;
+
+import static java.math.BigDecimal.*;
+
+@Configuration
+public class TestObjectFactory {
+
+    @Bean
+    public PaymentService paymentService() {
+        return new PaymentService(exRateProvider());
+    }
+
+    @Bean
+    public ExRateProvider exRateProvider() {
+        return new ExRateProviderStub(valueOf(1_000));
+    }
+}
