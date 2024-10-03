508b66b8a00aef7d64533f1af55edd342273c796
inkyu192 <inkyu192@gmail.com>
2024-10-03 18:40:07 +0900

    테스트와 DI (1)

--- src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
@@ -1,31 +1,34 @@
 package tobyspring.hellospring.payment;
 
 import org.junit.jupiter.api.Test;
-import tobyspring.hellospring.exrate.WebApiExRateProvider;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import static java.math.BigDecimal.TEN;
+import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PaymentServiceTest {
 
     @Test
-    void prepare() throws IOException {
-        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
-
-        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
-
-        // 환율정보 가져온다
-        assertThat(payment.getExRate()).isNotNull();
-
-        // 원화환산금액 계산
-        assertThat(payment.getConvertedAmount())
-                .isEqualTo(payment.getExRate().multiply(payment.getForeignCurrencyAmount()));
+    void convertedAmount() throws IOException {
+        testAmount(valueOf(500), valueOf(5_000));
+        testAmount(valueOf(1_000), valueOf(10_000));
+        testAmount(valueOf(3_000), valueOf(30_000));
 
         // 원화환산금액의 유효시간 계산
-        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
-        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+        // assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+        // assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+    }
+
+    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount) throws IOException {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate));
+
+        Payment payment = paymentService.prepare(1L, "USD", TEN);
+
+        assertThat(payment.getExRate()).isEqualByComparingTo(exRate);
+        assertThat(payment.getConvertedAmount()).isEqualByComparingTo(convertedAmount);
     }
 }
